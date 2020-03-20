08cf8096065a62e74890bd04921d19a1db67f8e3
Adam Young <ayoung@redhat.com>
2020-03-20 11:46:35 -0400

    User routes

--- app.R
+++ app.R
@@ -9,49 +9,63 @@
 
 library(shiny)
 
-# Define server logic for random distribution app ----
-server <- function(input, output) {
-    
-    # Reactive expression to generate the requested distribution ----
-    # This is called whenever the inputs change. The output functions
-    # defined below then use the value computed from this expression
-    d <- reactive({
-        dist <- switch(input$dist,
-                       norm = rnorm,
-                       unif = runif,
-                       lnorm = rlnorm,
-                       exp = rexp,
-                       rnorm)
-        
-        dist(input$n)
-    })
-    
-    # Generate a plot of the data ----
-    # Also uses the inputs to build the plot label. Note that the
-    # dependencies on the inputs and the data reactive expression are
-    # both tracked, and all expressions are called in the sequence
-    # implied by the dependency graph.
-    output$plot <- renderPlot({
-        dist <- input$dist
-        n <- input$n
-        
-        hist(d(),
-             main = paste("r", dist, "(", n, ")", sep = ""),
-             col = "#75AADB", border = "white")
-    })
-    
-    # Generate a summary of the data ----
-    output$summary <- renderPrint({
-        summary(d())
-    })
-    
-    # Generate an HTML table view of the head of the data ----
-    output$table <- renderTable({
-        head(data.frame(x = d()))
-    })
-    
+library(shiny.router)
+
+
+# This creates UI for each page.
+page <- function(title, content) {
+   div(
+       titlePanel(title),
+       p(
+           a(href="#!/home", "home"),
+           a(href="#!/side", "side"),
+           p(content),
+           uiOutput("power_of_input")
+       )
+   )
 }
-# Run the application 
+
+home_page <- page("Home page", "This is the home page!")
+side_page <- page("Side page", "This is the side page!")
 
 
-shinyApp(ui = htmlTemplate("www/index.html"), server)
+
+
+# Callbacks on the server side for the sample pages
+home_server <- function(input, output, session) {
+  output$power_of_input <- renderUI({
+    HTML(paste(
+      "I display <strong>square</strong> of input and pass result to <code>output$power_of_input</code>: ", 
+      as.numeric(input$int) ^ 2))
+  })
+}
+
+side_server <- function(input, output, session) {
+  output$power_of_input <- renderUI({
+    HTML(paste(
+      "I display <strong>cube</strong> of input and <strong>also</strong> pass result to <code>output$power_of_input</code>: ", 
+      as.numeric(input$int) ^ 3))
+  })
+}
+
+# Create routing. We provide routing path, a UI as well as a server-side callback for each page.
+router <- make_router(
+  route("/", home_page, home_server),
+  route("home", home_page, home_server),
+  route("side", side_page, side_server)
+)
+
+# Create output for our router in main UI of Shiny app.
+ui <- shinyUI(fluidPage(
+  shiny::sliderInput("int", "Choose integer:", -10, 10, 1, 1),
+  router_ui()
+))
+
+
+# Define server logic for random distribution app ----
+server <- function(input, output, session) {
+    router(input, output, session)
+}
+
+
+shinyApp(ui, server)
